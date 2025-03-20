5772db6941465707ffcfac969e317b0b6a982ce4
shoaibswe <shoaibswe@gmail.com>
2025-03-21 03:17:56 +0600

    fix: monthly product sales  in int layer

---
+++ intuswin_rs/models/intermediate/int_monthly_product_sales.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='table') }}
+
+WITH stg_sales AS (
+    SELECT *
+    FROM {{ ref('int_monthly_sales_metrics') }}
+),
+stg_products AS (
+    SELECT *
+    FROM {{ ref('stg_intus_product_catalogs') }}
+)
+
+SELECT
+    s.year AS transaction_year,
+    s.month AS transaction_month,
+    p.category,
+    s.unique_customers,
+    s.total_revenue,
+    s.total_cost
+FROM stg_sales s
+LEFT JOIN stg_products p ON s.product_id = p.product_id
