3a5aab3c238679601de307f068989feb19ac7179
shoaibswe <shoaibswe@gmail.com>
2025-03-21 03:16:24 +0600

    fix: monthly sales metric in int layer

---
+++ intuswin_rs/models/intermediate/int_monthly_sales_metrics.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+WITH stg_sales AS (
+    SELECT *
+    FROM {{ ref('stg_intus_sales_transactions') }}
+)
+
+SELECT
+    {{ extract_year_month('transaction_timestamp') }},
+    product_id,
+    COUNT(DISTINCT user_id) AS unique_customers,
+    SUM(revenue) AS total_revenue,
+    SUM(cost) AS total_cost
+FROM stg_sales
+GROUP BY 1, 2, 3
