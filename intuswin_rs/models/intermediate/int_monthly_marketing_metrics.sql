a572e56410bb0034f393b8f69733d437e6f89acf
shoaibswe <shoaibswe@gmail.com>
2025-03-21 03:16:56 +0600

    fix: monthly marketing  metrics in int layer

---
+++ intuswin_rs/models/intermediate/int_monthly_marketing_metrics.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table') }}
+
+WITH stg_marketing AS (
+    SELECT *
+    FROM {{ ref('stg_intus_marketing_events') }}
+)
+
+SELECT
+    {{ extract_year_month('event_timestamp') }},
+    user_id,
+    SUM(cost) AS total_marketing_cost,
+    COUNT(event_id) AS total_marketing_events
+FROM stg_marketing
+GROUP BY 1, 2
